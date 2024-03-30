533942dd1af05d26501390014891d218400885f1
Zeljana <zeljana.milosevic@fon.bg.ac.rs>
2024-03-30 16:46:14 +0100

    1. Inicijalizacija Java aplikacije koristeci Spring Boot

---
+++ src/main/java/com/heliant/HeliantApplication.java
@@ -0,0 +1,13 @@
+package com.heliant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliant/HeliantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
